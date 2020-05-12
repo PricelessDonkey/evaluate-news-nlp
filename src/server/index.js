e191028d6f73c26429e9e7a9f097992b844e997b
Sam Drilias <sam@sams-air.attlocal.net>
2020-05-11 19:41:31 -0500

    adding gitignore

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
 
 const app = express()
 
@@ -8,9 +12,14 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-    //res.sendFile(path.resolve('src/client/views/index.html'))
+    //res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
