e37dbf9a2c9be7a1da2d7f0efefe592918e0a797
Sam Drilias <sam@sams-air.attlocal.net>
2020-05-18 20:48:34 -0500

    working routes

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,45 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let userEntry = document.getElementById('name').value
+    Client.checkForName(userEntry) // check for valid URL first
+    
+    postData('/submit', { urlString: userEntry })
+        .then(() => updateUI())
+        .catch(function (error) {
+            alert(error)
+        })
+};
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+// post data
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
     })
+    if (response.status != 201) throw new Error('bad request');
+  }
+  
+const updateUI = async () => {
+    
+    const request = await fetch('http://localhost:8080/all');
+    try {
+        console.log('request: ' + request);
+        const allData = await request.json();
+        console.log('allData: ' + allData);
+
+        const index = allData.length - 1;
+        
+     
+        document.getElementById('results').innerHTML = allData[index].polarity;
+      
+      } catch (error) {
+        console.log("error", error);
+      }
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -5,21 +5,33 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 var aylien = require("aylien_textapi");
-
+const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static('dist'))
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
 console.log(__dirname)
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = [];
 
 var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
-    });
+})
 
 app.get('/', function (req, res) {
-    //res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -27,6 +39,35 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.post('/submit', processUrl);
+
+function processUrl(req, res) {
+    let reqBody = req.body;
+    let urlString = reqBody.urlString;
+    
+    textapi.sentiment({
+        'url': urlString
+    }, function(error, response) {
+        if (error === null) {
+            addResponse(response);
+            res.status(201).send('OK');
+        } else {
+            res.status(500).send(error);
+        }
+    })
+}
+
+function addResponse(response) {
+    let newEntry = {
+        polarity: response.polarity,
+        subjectivity: response.subjectivity
+    }
+
+    projectData.push(newEntry);
+}
+
+app.get('/all', sendData);
+  
+function sendData(request, response) {
+    response.send(projectData);
+}
